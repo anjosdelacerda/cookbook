e748fde360e1b0975807fd6301b923062c1d2fa2
anjosdelacerda <anjosdelacerda@gmail.com>
2023-02-21 18:03:36 -0300

    CHORE: divisão das funcionalidades de visualizar lista de receitas e adição de receitas, inclusão da opção sair do app

--- cookbook.rb
+++ cookbook.rb
@@ -1,4 +1,53 @@
 puts "Bem-vindo ao Cookbook, a sua rede social de gerenciamento de receitas!"
-puts "Digite o nome da sua receita: "
-gets 
-puts "Receita cadastrada com sucesso!"
+receitas = []
+puts "=== MENU ==="
+puts 
+puts "[1] digite para para cadastrar uma receita "
+puts "[2] digite para ver todas as receitas3 "
+puts "[3] digite para sair"
+
+print "digite uma opção:"
+gets.chomp()
+
+
+opcao = gets.to_i()
+# puts "#{opcao} ola nando"
+
+while (opcao != 3) do
+	if(opcao == 1)
+		print "digite a sua receita: "
+		receita = gets.chomp()
+
+
+		receitas << receita 
+
+		puts "A receita #{receita} foi cadastrada com sucesso"
+	elsif(opcao == 2)
+		puts 
+		puts "=== Minhas Receitas ==="
+		puts 
+		
+		receitas.each do |receitinha| 
+			puts receitinha 
+		end
+	else 
+		puts "opção invalida"
+	end
+	puts "=== MENU ==="
+	puts 
+	puts "[1] digite para para cadastrar uma receita "
+	puts "[2] digite para ver todas as receitas3 "
+	puts "[3] digite para sair"
+
+	print "digite uma opção:"
+	gets.chomp()
+	opcao = gets.to_i()
+end 
+
+
+
+
+
+puts "Até logo, tchau"
+
+
